ee04add6e226369f3460f181214816fd7abdea5d
ibrahimnjoya677 <63928990+ibrahimnjoya677@users.noreply.github.com>
2020-05-13 15:58:12 +0200

    envoi de mes fichiers existants

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Digital School</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body id="top">
+  <header class="header">
+    <div class="logo">
+      <a href="index.php">
+        <span class="logo-img"><img src="images/log.png" alt="logo">VirtualSchool</span>
+      </a>
+    </div>
+    <nav class="header-nav">
+      <ul class="menu">
+        <li><a href="index.php" class="active">Acceuil</a></li>
+        <li><a href="rubriques/Classes.php">Classes</a></li>
+        <li><a href="rubriques/Barbillards.php">Barbillard</a></li>
+        <li><a href="rubriques/contact.php">Administration</a></li>
+        <li><a href="rubriques/inscriptions.php" class="btn-contact">Inscription</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section class="banner">
+    <div class="banner-img"></div>
+    <div class="banner-info">
+      <div class="b-tittle">"Let's consider, that any treatment must take into account the possibility of the
+        ultimate advantage of</div>
+      <p>
+        "Let's consider, that any treatment must take into account the possibility of the ultimate advantage
+        ofunt. It should rather be regarded as an integral part of the integration prospects.
+        s a matter of fact aspects of the hardware maintenance is highly considerable.
+      </p>
+      <div class="b-btn">
+        <a href="#info">
+          <span>
+            <img class="angle-down" src="images/angle-down.svg">
+          </span>Voir Plus
+        </a>
+      </div>
+    </div>
+  </section>
+  <section class="banner2" id="info">
+    <div class="b2-title">
+      Qui somme nous ?
+    </div>
+    <div class="b2-items">
+      <div class="items ">
+        <div class="items-title">Nos instructions</div>
+        <p>
+          "Notwithstanding that the structure of the arguments and claims cannot be developed under such
+          circumstances. It is undeniable that the unification of the integration prospects provides a deep
+          insight into The Index of Professional Bounce"
+        </p>
+      </div>
+      <div class="items ">
+        <div class="items-title">A propos de nous </div>
+        <p>
+          "Notwithstanding that the structure of the arguments and claims cannot be developed under such
+          circumstances. It is undeniable that the unification of the integration prospects provides a deep
+          insight into The Index of Professional Bounce"
+        </p>
+      </div>
+      <div class="items">
+        <div class="items-title">Information</div>
+        <p>
+          "Notwithstanding that the structure of the arguments and claims cannot be developed under such
+          circumstances. It is undeniable that the unification of the integration prospects provides a deep
+          insight into The Index of Professional Bounce"
+        </p>
+      </div>
+    </div>
+  </section>
+  <section class="banner3">
+    <div class="banner-info">
+      <div class="b-tittle">"Let's consider, that any treatment must take into account the possibility of the
+        ultimate advantage of</div>
+      <p>
+        "Let's consider, that any treatment must take into account the possibility of the ultimate advantage
+        ofunt. It should rather be regarded as an integral part of the integration prospects.
+        s a matter of fact aspects of the hardware maintenance is highly considerable.
+      </p>
+    </div>
+    <div class="b3-btn">
+      <a href="pages/cours.php">Rejoindre sa salle</a>
+      <a href="pages/evaluation.php" class="is-color">Consulter le barbillard</a>
+    </div>
+  </section>
+  <section class="banner4">
+    <div class="b2-title">
+      Nos partenaires
+    </div>
+
+    <!--Partenaires-->
+    <section class="partenaires">
+        <div ><img src="images/iuc.jpg" alt="logo"></div>
+        <div><img src="images/sanau.jpg" alt="logo"></div>
+        <div><img src="images/php.jpg" alt="logo"></div>
+    </section>
+  </section>
+
+  <footer class="footer">
+    <div class="newsletter ">
+      <div class="newsletter-info">
+        <div class="n-title">NEWSLETTER</div>
+        <p>
+          "Notwithstanding that the structure of the arguments and claims cannot be developed under such
+          circumstances. It is undeniable that the unification of the integration prospects provides a deep
+          insight into The Index of Professional Bounce"
+        </p>
+      </div>
+      <div class="newsletter-form">
+        <div class="n-form">
+          <form action="" class="form" method="GET">
+            <div class="form-group">
+              <input type="text" name="name" id="name" class="form-control" placeholder="Nom et Prenom">
+            </div>
+            <div class="form-group">
+              <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+            </div>
+            <button type="submit" class="btn btn1">
+              S'inscrire
+            </button>
+          </form>
+        </div>
+        <div class="newsletter-img">
+          <img src="images/envelope.svg" alt="newsletter-images">
+        </div>
+      </div>
+    </div>
+    <div class="foot">
+      <div class="footer-logo">
+        <a href="index.html">
+          <span class="logo-im"><img class = "fin" src="images/log.png" alt="logo">virtualschool</span>
+        </a>
+      </div>
+      <div class="footer-text">
+      <pre>  <a href="#">Autorisation d'ouverture</a>/ <a href="#">politique de confidentialite</a> </pre>
+      <h5>copyrigth 2020.interiction formel de reproduction</h5>
+      </div>
+      <div class="footer-btn">
+        <a href="index.php">Go up</a>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
